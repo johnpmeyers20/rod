5c0043eb2d769084ac86a6191e11796fe9b3d4ee
JP Meyers <johnpmeyers20@yahoo.com>
2020-10-22 14:29:35 -0400

    added ArticlesList

--- client/src/components/ArticlesList.jsx
+++ client/src/components/ArticlesList.jsx
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 export default function ArticlesList(props) {
   return (
     <div>
-      {props.cats.map(cat => (
-        <React.Fragment key={cat.id}>
-          <Link to={`/cats/${cat.id}`}><h3>{cat.name}</h3></Link>
+      {props.articles.map(article => (
+        <React.Fragment key={article.id}>
+          <Link to={`/articles/${article.id}`}><h3>{article.title}</h3></Link>
         </React.Fragment>
       ))}
-      <Link to="/cats/new"><button>Add an cat</button></Link>
+      <Link to="/articles/new"><button>Add an article</button></Link>
     </div>
   )
 };
